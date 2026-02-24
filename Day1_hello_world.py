c128de75fd6b9cf7b5ff0d917669adfe6340e357
harikaranabba <harikaranabba@gmail.com>
2026-02-24 21:52:03 +0530

    Add files via upload

---
+++ Day1_hello_world.py
@@ -0,0 +1,10 @@
+#End argument
+print('105+30',end='=')
+print(105+30)
+
+print('waterbottle','pencil','speakers','mouse')
+print('waterbottle','pencil','speakers','mouse',sep='!')
+
+print("OMG","there is a lion",sep="! ",end=',')
+print("really?")
+    
