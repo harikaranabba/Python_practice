b60135ada4b33a189f38c381b99d7a75163b3528
harikaranabba <harikaranabba@gmail.com>
2026-02-25 20:23:21 +0530

    Day2_escape_sequence.py

---
+++ Day2_Escape_sequence.py
@@ -0,0 +1,19 @@
+#Escape sequence
+#\n
+print("January February")
+print("January\nFebruary")
+
+#\t
+print("JanuaryFebruary")
+print("January\t\tFebruary")
+
+#\
+print('hey rook! he\'s my brother')
+
+
+print("Hari said \"No Pain, No Gain\"")
+
+print('Hari said "No Pain, No Gain"')
+
+#\n\t
+print("'it's me'\n\t\t-Viswanath Anand")
