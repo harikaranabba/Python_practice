2c27e18b119cb00c8d15889c1abec99a19f461bf
harikaranabba <harikaranabba@gmail.com>
2026-02-25 20:21:37 +0530

    Update Day1_hello_world.py

--- Day1_hello_world.py
+++ Day1_hello_world.py
@@ -2,9 +2,12 @@
 print('105+30',end='=')
 print(105+30)
 
+#sepration
 print('waterbottle','pencil','speakers','mouse')
 print('waterbottle','pencil','speakers','mouse',sep='!')
 
+#SEPRATION AND END ARUGUMENT
 print("OMG","there is a lion",sep="! ",end=',')
 print("really?")
+
     
