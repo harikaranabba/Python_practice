a5dff7623fb1ab9035e9c4bcd2e5d52e829de5c3
harikaranabba <harikaranabba@gmail.com>
2026-03-02 00:14:00 +0530

    Add files via upload

---
+++ Day5_logical operator.py
@@ -0,0 +1,51 @@
+ 
+ #logical statement
+a=10
+b=10
+statement1=(a==10)
+statement2=(b==10)
+
+#print(statement1 and statement2)
+#print(statement1 or statement2)
+
+print(not(statement1))
+print(not(statement2))
+
+#logical quiz
+
+num=10
+print(num>5 and num<15)
+print(10%2==0 or 10%3==0)
+print(not(num==0))
+
+#bitwise operater
+a=2
+b=3
+print(a&b)
+print(a|b)
+
+#bitwise (right shift , left shift operater)
+a=2
+b=3
+print(a>>1)
+
+#QUIZ
+
+X=10
+Y=4
+print(X&Y)
+print(X|Y)
+print(X^Y)
+
+#string replication and concatenation
+stars="*"
+greeting="happy"+"journey"
+print(stars*20)
+print(greeting)
+print(stars*20)
+
+# right shift
+a=2
+b=3
+print(a>>1)
+print(b>>1)
