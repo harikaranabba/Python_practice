a783b9770b70493a3760a2343497753b93783c33
harikaranabba <harikaranabba@gmail.com>
2026-03-03 20:52:43 +0530

    Add files via upload

---
+++ Day7_Type_casting.py
@@ -0,0 +1,46 @@
+#type casting
+num=float(input("enter a number:"))
+print(type(num))
+#average of marks
+mark1=float(input('enter maths mark:'))
+mark2=float(input('enter physic mark:'))
+mark3=float(input('enter chemistry mark:'))
+total_marks=mark1+mark2+mark3
+avg_mark=total_marks/3
+print(avg_mark)
+
+# quiz
+a=int(input("value a:"))
+b=int(input("value b:"))
+c=int(input("value c:"))
+print(3*a**2)
+total_value=3*a**2+b-2*c
+print(total_value)
+  
+a=5
+n=25
+b=float(a)
+s=str(25)
+print(type(b),b)
+print(type(s),s)
+
+#string rebulication
+num=float(input("enter a number:"))
+print(type(num))
+ 
+ # negative indexing
+number=(input("enter_the_Value:"))
+print(number[len(number)-1])
+#or
+print(number[-1]) 
+
+number=(input("enter_the_Value:"))
+number=10
+print(number%10)
+
+
+
+
+
+
+
