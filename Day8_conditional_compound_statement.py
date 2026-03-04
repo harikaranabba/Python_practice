1f12a1a6f6d4653bae0aac6a9750c51801a6a11d
harikaranabba <harikaranabba@gmail.com>
2026-03-04 21:55:38 +0530

    Add files via upload

---
+++ Day8_conditional_compound_statement.py
@@ -0,0 +1,30 @@
+#conditional statement
+#compound statement
+a= 10
+b= int(input("b:"))
+if (a==b):
+    print('a is to equal to b')
+    print('the statement is true')
+elif(b==15):
+    print("b is 15")
+else:
+    print('a is not equal to b')
+    print('the statement is false')
+
+#quiz
+Tenure=int(input('tenure in the months:'))
+if(Tenure>=12):
+    print("we are giving upto 24 months tenure")
+    print('7% intrest rate')
+else:
+    print('we are giving uptp 12 months tenure')
+    print('18% intres rate ')
+print("offer ongoing now")
+
+
+    
+
+
+
+    
+
