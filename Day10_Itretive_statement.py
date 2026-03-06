2a4be4090e79db49f0f035e92e33b78a37cfec8a
harikaranabba <harikaranabba@gmail.com>
2026-03-06 21:02:18 +0530

    Add files via upload

---
+++ Day10_Itretive_statement.py
@@ -0,0 +1,41 @@
+"""#Itretive_statement
+#syntax
+num=5
+for i in range(num):
+    print(i)
+#starting_&_ending_value
+for i in range(10,50):
+    print(i)
+#starting, ending & shifted_value
+for i in range(10,50,-1):
+    print(i)
+
+# quizz
+for i in range(5, 11, 2):
+    print(i,end="")
+
+#while loop
+#syntax
+while (condition):
+    statement(s)
+
+#example
+num=5
+while(num>0):
+    print("hari")
+    num-=1
+"""
+#quiz
+count=10
+while count<10:
+    print(count)
+    count+=1
+    
+
+
+
+
+
+    
+
+
